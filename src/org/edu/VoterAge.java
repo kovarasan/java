073916fc67463b054d4fa611be0efa4cd28b226c
kovarasan <ko.arasan95@gmail.com>
2022-01-05 19:30:05 +0530

    java

---
+++ src/org/edu/VoterAge.java
@@ -0,0 +1,14 @@
+package org.edu;
+
+public class VoterAge {
+	public static void main(String[] args) {
+		int age = 18;
+		if (age>=18) {
+			System.out.println("Eligible to vote");
+		} else {
+			System.out.println("Not Eligible to vote");
+
+		}
+	}
+
+}
